3513c2b11238409e556df546daae4f2ea1c1e390
Brad McCollum <btmcco8@gmail.com>
2018-08-14 21:18:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash) #will need to iterate through the hash, compare values, and then return the key with the smallest value
 min_value = ""
   lowest_key = nil
-  lowest_value = INFINITY
+  lowest_value = Float::INFINITY
 
   name_hash.each do |key, value|
     if value < lowest_value

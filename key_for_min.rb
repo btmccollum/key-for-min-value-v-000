59706f34a6bdc8c305d8735e392f33fb07cb8e77
Brad McCollum <btmcco8@gmail.com>
2018-08-14 21:20:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash) #will need to iterate through the hash, compare values, and then return the key with the smallest value
 min_value = ""
   lowest_key = nil
-  lowest_value =50
+  lowest_value = 50000000
 
   name_hash.each do |key, value|
     if value < lowest_value
